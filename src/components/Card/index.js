238fbe9aa1820d8ca0fad7a9db643d5a2d77bb7f
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-14 16:12:59 -0500

    added content to the card js file to further reduce code redundency and simplify organization on the about.js page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Container from "../Container";
 
 
 function Card(props) {
     return (
-
+        <div className="card text-white bg-dark mb-3">
+            <h1 className="card-header">About Me</h1>
+            <div className="card-body">{props.children}</div>
+        </div>
     );
 }
 
