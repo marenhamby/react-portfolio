5a2e6f5c22768c4a684dffba8a53c23eafcdb076
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-14 15:44:09 -0500

    added photos for the portfolio page into the public folder and tried to link to the portfolio page. added new card folder for components to further break down pages. added content to the footer css file to test it's working

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-
 function About() {
     return (
         <div className="container">
-        <div className="card text-white bg-dark mb-3" style="max-width: auto">
+        <div className="card text-white bg-dark mb-3">
+             {/* style="max-width: auto" */}
             <h1 className="card-header">Portfolio</h1>
             <div className="card-body">
                 <div className="row">
                     <div className="col">
                         <h4>The Productivity Planner</h4>
-                        <img src="./Assets/images/planner-dashboard.png" alt="planner webpage">
+                        <img src="img/plannerDashboard.png" alt="planner webpage">
                              {/* style="width: 100%" style="margin: 16px" */}
                         <h5>
                             {/* <a href="https://obscure-lake-47661.herokuapp.com/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
@@ -22,7 +22,7 @@ function About() {
                     </div>
                     <div className="col">
                         <h4>The Work Day Scheduler</h4>
-                        <img src="./Assets/images/schedule_page_screenshot.PNG" alt="Scheduler webpage"> 
+                        <img src="img/schedule_page_screenshot.PNG" alt="Scheduler webpage"> 
                         {/* style="width: 100%" style="margin: 16px" */}
                         <h5>
                             {/* <a href="https://marenhamby.github.io/work-scheduler/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
@@ -34,7 +34,7 @@ function About() {
                     </div>
                     <div className="col">
                         <h4>The UltiMutt Dog Finder</h4>
-                        <img src="./Assets/images/homePage.png" alt="dog finder webpage">
+                        <img src="img/homePage.png" alt="dog finder webpage">
                              {/* style="width: 100%" style="margin: 16px" */}
                         <h5>
                             {/* <a href="https://marenhamby.github.io/UltiMutt_dog_finder/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
