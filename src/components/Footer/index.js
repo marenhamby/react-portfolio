5f6dfa3b7a09b9dcdad68eb996285f24b84ff162
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-14 09:56:25 -0500

    added content to footer js to add footer to the pages

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Made for you!</span>
+    </footer>
+  );
+}
+
+export default Footer;
