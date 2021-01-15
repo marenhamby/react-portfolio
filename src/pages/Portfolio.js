56952565ccf9019e7d0a93e2dced0198d83c2bec
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-14 21:27:46 -0500

    added links to the deployed page and the code page for the productivity planner project on the portfolio page. updated style to match colors for the links and added the noreferrer noopener to the target blanks

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -13,10 +13,10 @@ function Portfolio() {
                             <h4>The Productivity Planner</h4>
                             <img src="image/planner-dashboard.png" alt="planner webpage" className="portImage"></img>
                             <h5>
-                                {/* <a href="https://obscure-lake-47661.herokuapp.com/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
+                                <a href="https://obscure-lake-47661.herokuapp.com/" target="_blank" rel="noreferrer noopener" className="link">Link to the deployed page here!</a>
                             </h5>
                             <h5>
-                                {/* <a href="https://github.com/averyjbrown2/Project-2" target="_blank" style="color: #A8BCD4">Link to the code here!</a> */}
+                                <a href="https://github.com/averyjbrown2/Project-2" target="_blank" rel="noreferrer noopener" className="link">Link to the code here!</a>
                             </h5>
 
                         </div>
