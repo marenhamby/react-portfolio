6a00c02b3699e595c741b11c86f96a1786f3a90a
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-14 20:56:46 -0500

    added container and card components to the contact.js file to reduce redundency of code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,36 +1,37 @@
 import React from "react";
+import Card from "../components/Card";
+import Container from "../components/Container";
 
 
 function About() {
     return (
-        <div className="container">
-        <div className="card text-white bg-dark mb-3">
-        {/* // style="max-width: auto;" */}
-           <h1 className="card-header">Contact</h1>
-           <div className="card-body">
-               <div className="form-group">
-                   <h3>Email:</h3>
-                   <h4> 
-                       {/* style="color: #A8BCD4" */}
+        <div>
+            <Container>
+                <Card>
+                    <div className="form-group">
+                        <h3>Email:</h3>
+                        <h4>
+                            {/* style="color: #A8BCD4" */}
                        maren.hamby@gmail.com</h4>
-               </div>
-               <div className="form-group">
-                    <h3>LinkedIn:</h3>
-                    <h4>
-                         {/* style="color: #A8BCD4"> */}
-                        {/* <a href="https://linkedin.com/in/maren-hamby-90a924ba" target="_blank" style="color: #A8BCD4">Click Me!</a> */}
-                    </h4> 
-               </div>
-               <div className="form-group">
-                    <h3>GitHub:</h3>
-                    <h4>
-                         {/* style="color: #A8BCD4" */}
-                        {/* <a href="https://github.com/marenhamby" target="_blank" style="color: #A8BCD4">Click Me!</a> */}
-                    </h4>
-                </div>
-            </div>
+                    </div>
+                    <div className="form-group">
+                        <h3>LinkedIn:</h3>
+                        <h4>
+                            {/* style="color: #A8BCD4"> */}
+                            {/* <a href="https://linkedin.com/in/maren-hamby-90a924ba" target="_blank" style="color: #A8BCD4">Click Me!</a> */}
+                        </h4>
+                    </div>
+                    <div className="form-group">
+                        <h3>GitHub:</h3>
+                        <h4>
+                            {/* style="color: #A8BCD4" */}
+                            {/* <a href="https://github.com/marenhamby" target="_blank" style="color: #A8BCD4">Click Me!</a> */}
+                        </h4>
+                    </div>
+                </Card>
+            </Container>
         </div>
-        </div>
+
     );
 }
 
