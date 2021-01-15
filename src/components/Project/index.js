13c02efceff62772db7410633d9c41b5e89d4c95
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-14 22:22:39 -0500

    added info to portfolio page to be able to cycle through the portfolio information and display them dynamically using the project component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,13 +4,13 @@ import React from "react";
 function Project(props) {
     return (
         <div className="col">
-            <h4>The Productivity Planner</h4>
-            <img src="image/planner-dashboard.png" alt="planner webpage" className="portImage"></img>
+            <h4>{props.title}</h4>
+            <img src={props.image} alt={props.alt} className="portImage"></img>
             <h5>
-                <a href="https://obscure-lake-47661.herokuapp.com/" target="_blank" rel="noreferrer noopener" className="link">Link to the deployed page here!</a>
+                <a href={props.deployed} target="_blank" rel="noreferrer noopener" className="link">Link to the deployed page here!</a>
             </h5>
             <h5>
-                <a href="https://github.com/averyjbrown2/Project-2" target="_blank" rel="noreferrer noopener" className="link">Link to the code here!</a>
+                <a href={props.code} target="_blank" rel="noreferrer noopener" className="link">Link to the code here!</a>
             </h5>
 
         </div>
@@ -18,3 +18,4 @@ function Project(props) {
 }
 
 export default Project;
+
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,54 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "../components/Card";
 import Container from "../components/Container";
 import Row from "../components/Row";
+import Project from "../components/Project";
 import "./portfolio.css";
 
-function Portfolio() {
-    return (
-        <div>
-            <Container>
-                <Card>
-                    <Row>
-                        <div className="col">
-                            <h4>The Productivity Planner</h4>
-                            <img src="image/planner-dashboard.png" alt="planner webpage" className="portImage"></img>
-                            <h5>
-                                <a href="https://obscure-lake-47661.herokuapp.com/" target="_blank" rel="noreferrer noopener" className="link">Link to the deployed page here!</a>
-                            </h5>
-                            <h5>
-                                <a href="https://github.com/averyjbrown2/Project-2" target="_blank" rel="noreferrer noopener" className="link">Link to the code here!</a>
-                            </h5>
 
-                        </div>
-                        <div className="col">
-                            <h4>The Work Day Scheduler</h4>
-                            <img src="image/schedule_page_screenshot.PNG" alt="Scheduler webpage" className="portImage"></img>
-                            <h5>
-                                <a href="https://marenhamby.github.io/work-scheduler/" target="_blank" rel="noreferrer noopener" className="link">Link to the deployed page here!</a>
-                            </h5>
-                            <h5>
-                                <a href="https://github.com/marenhamby/work-scheduler" target="_blank" rel="noreferrer noopener" className="link">Link to the code here!</a>
-                            </h5>
 
-                        </div>
-                        <div className="col">
-                            <h4>The UltiMutt Dog Finder</h4>
-                            <img src="image/homePage.png" alt="dog finder webpage" className="portImage"></img>
-                            <h5>
-                                <a href="https://marenhamby.github.io/UltiMutt_dog_finder/" target="_blank" rel="noreferrer noopener" className="link">Link to the deployed page here!</a>
-                            </h5>
-                            <h5>
-                                <a href="https://github.com/marenhamby/UltiMutt_dog_finder" target="_blank" rel="noreferrer noopener" className="link">Link to the code here!</a>
-                            </h5>
 
-                        </div>
-                    </Row>
-                </Card>
-            </Container>
-        </div>
+class Portfolio extends Component {
 
-    );
+    state = [{
+        title: "The Productivity Planner",
+        image: "image/planner-dashboard.png",
+        alt: "planner webpage",
+        deployed: "https://obscure-lake-47661.herokuapp.com/",
+        code: "https://github.com/averyjbrown2/Project-2",
+    },
+    {
+        title: "The Work Day Scheduler",
+        image: "image/schedule_page_screenshot.PNG",
+        alt: "scheduler webpage",
+        deployed: "https://marenhamby.github.io/work-scheduler/",
+        code: "https://github.com/marenhamby/work-scheduler",
+    },
+    {
+        title: "The UltiMutt Dog Finder",
+        image: "image/homePage.png",
+        alt: "dog finder webpage",
+        deployed: "https://marenhamby.github.io/UltiMutt_dog_finder/",
+        code: "https://github.com/marenhamby/UltiMutt_dog_finder",
+    },
+    ];
+
+    //   loadProjects = state => {
+    //       for (i=0, i< state.length, i++) {
+    //         return {
+    //             title= state[i].title,
+    //             image= state[i].image,
+    //             alt= state[i].alt,
+    //             deployed= state[i].deployed,
+    //             code= state[i].code
+    //         }
+    //       }
+    //       });
+    //   }
+
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Card>
+                        <Row>
+                            <Project
+                                title={this.state.title}
+                                image={this.state.image}
+                                alt={this.state.alt}
+                                deployed={this.state.deployed}
+                                code={this.state.code}
+                            ></Project>
+                        </Row>
+                    </Card>
+                </Container>
+            </div >
+
+        );
+
+
+    }
 }
 
 export default Portfolio;
