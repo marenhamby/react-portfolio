30b278d2a7a1aec8df83da9abb2bea2c50a80c6b
marenh <72942304+marenhamby@users.noreply.github.com>
2021-01-14 21:20:53 -0500

    reconnected the images for the past projects in the portfolio.js. adjusted style components in the portfolio.css document

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -11,39 +11,36 @@ function Portfolio() {
                     <div className="row">
                         <div className="col">
                             <h4>The Productivity Planner</h4>
-                            <img src="img/plannerDashboard.png" alt="planner webpage">
-                                {/* style="width: 100%" style="margin: 16px" */}
-                                <h5>
-                                    {/* <a href="https://obscure-lake-47661.herokuapp.com/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
-                                </h5>
-                                <h5>
-                                    {/* <a href="https://github.com/averyjbrown2/Project-2" target="_blank" style="color: #A8BCD4">Link to the code here!</a> */}
-                                </h5>
-                            </img>
+                            <img src="image/planner-dashboard.png" alt="planner webpage" className="portImage"></img>
+                            <h5>
+                                {/* <a href="https://obscure-lake-47661.herokuapp.com/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
+                            </h5>
+                            <h5>
+                                {/* <a href="https://github.com/averyjbrown2/Project-2" target="_blank" style="color: #A8BCD4">Link to the code here!</a> */}
+                            </h5>
+
                         </div>
                         <div className="col">
                             <h4>The Work Day Scheduler</h4>
-                            <img src="img/schedule_page_screenshot.PNG" alt="Scheduler webpage">
-                                {/* style="width: 100%" style="margin: 16px" */}
-                                <h5>
-                                    {/* <a href="https://marenhamby.github.io/work-scheduler/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
-                                </h5>
-                                <h5>
-                                    {/* <a href="https://github.com/marenhamby/work-scheduler" target="_blank" style="color: #A8BCD4">Link to the code here!</a> */}
-                                </h5>
-                            </img>
+                            <img src="image/schedule_page_screenshot.PNG" alt="Scheduler webpage" className="portImage"></img>
+                            <h5>
+                                {/* <a href="https://marenhamby.github.io/work-scheduler/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
+                            </h5>
+                            <h5>
+                                {/* <a href="https://github.com/marenhamby/work-scheduler" target="_blank" style="color: #A8BCD4">Link to the code here!</a> */}
+                            </h5>
+
                         </div>
                         <div className="col">
                             <h4>The UltiMutt Dog Finder</h4>
-                            <img src="img/homePage.png" alt="dog finder webpage">
-                                {/* style="width: 100%" style="margin: 16px" */}
-                                <h5>
-                                    {/* <a href="https://marenhamby.github.io/UltiMutt_dog_finder/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
-                                </h5>
-                                <h5>
-                                    {/* <a href="https://github.com/marenhamby/UltiMutt_dog_finder" target="_blank" style="color: #A8BCD4">Link to the code here!</a> */}
-                                </h5>
-                            </img>
+                            <img src="image/homePage.png" alt="dog finder webpage" className="portImage"></img>
+                            <h5>
+                                {/* <a href="https://marenhamby.github.io/UltiMutt_dog_finder/" target="_blank" style="color: #A8BCD4">Link to the deployed page here!</a> */}
+                            </h5>
+                            <h5>
+                                {/* <a href="https://github.com/marenhamby/UltiMutt_dog_finder" target="_blank" style="color: #A8BCD4">Link to the code here!</a> */}
+                            </h5>
+
                         </div>
                     </div>
                 </Card>
